4f1a26f0171ae49210843f3599be0477250ae55b
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-15 15:02:25 +0500

    Refactor index page: replace placeholder text with ProjectGrid component and section header for selected work, enhancing layout and functionality.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,89 +1,40 @@
 <script lang="ts">
+
   import Hero from "../components/Hero.svelte";
-</script>
+    import ProjectGrid from "../components/ProjectGrid.svelte";
+    import {projects } from  "../lib/data/projects";
+    import { p } from 'sv-router/generated';
+ </script>
 
 <Hero />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
+<section>
+  <div class="section-header">
+    <h2 class="section-title">selected work</h2>
+    <a href={p('/project')} class="see-all">see all →</a>
+  </div>
+  <ProjectGrid {projects} limit={2} />
+</section>
 
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-/><br /><br /><br /><br /><br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
-
-<br /><br />
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nemo quia
-  sit officiis reprehenderit deleniti omnis aliquam, quae a molestiae
-  consequuntur assumenda quos atque nisi, quasi minus? Architecto, sequi totam.
-</p>
+<style>
+  .section-header {
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    margin-bottom: 1.5rem;
+  }
+  .section-title {
+    font-size: 0.75rem;
+    font-weight: 600;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: var(--text-muted);
+    margin: 0;
+  }
+  .see-all {
+    font-size: 0.85rem;
+    color: var(--text-secondary);
+    text-decoration: none;
+    transition: color 0.2s;
+  }
+  .see-all:hover { color: var(--text-primary); }
+</style>
