2c77a6b234584665e26003f959af68b946347723
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-16 11:22:54 +0500

    Implement theme management: add logic to toggle dark mode based on user preference and local storage.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { mount } from 'svelte';
 import '../app.css'
 import App from './App.svelte';
+const stored = localStorage.getItem('theme');
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const theme = stored ?? (prefersDark ? 'dark' : 'light');
+document.documentElement.classList.toggle('dark', theme === 'dark');
 
 mount(App, { target: document.querySelector('#app')! });
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import { router } from 'sv-router/vite-plugin';
 import { defineConfig } from 'vite';
-
+import path from 'path';
 export default defineConfig({
 	plugins: [svelte(), router(), tailwindcss()],
+	resolve: {
+		alias: {
+		  '@': path.resolve(__dirname, './src'),
+		},
+	  },
 });
