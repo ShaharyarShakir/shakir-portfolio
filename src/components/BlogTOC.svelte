c802a25782f49955e2bb8e8f6696c1a91d6d6ce2
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-18 00:33:21 +0500

    refactor: restructure project routes and components to a modular architecture

---
+++ src/components/BlogTOC.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import BlogSidebar from './BlogSidebar.svelte';
+  import type { TocItem } from '$lib/utils/parseMarkdown';
+
+  interface Props {
+    toc: TocItem[];
+  }
+
+  let { toc }: Props = $props();
+</script>
+
+<BlogSidebar {toc} />
