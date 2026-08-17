6c74d02ed54c348de2893c35775e85566aa56bb0
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-18 00:32:49 +0500

    feat: migrate project structure to SvelteKit and add blog navigation components

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { router } from 'sv-router/vite-plugin';
 import { defineConfig } from 'vite';
-import path from 'path';
+
 export default defineConfig({
-	plugins: [svelte(), router(), tailwindcss()],
-	resolve: {
-		alias: {
-		  '@': path.resolve(__dirname, './src'),
-		},
-	  },
+	plugins: [sveltekit(), tailwindcss()]
 });
